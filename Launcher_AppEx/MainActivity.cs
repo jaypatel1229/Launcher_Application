be855ee28ba9069b8b1def4594e39da8a1407a82
jay.patel <jay.patel@1R-80>
2022-02-10 16:19:07 +0530

    Add project files.

---
+++ Launcher_AppEx/MainActivity.cs
@@ -0,0 +1,67 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace Launcher_AppEx
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme",MainLauncher =true)]
+    class MainActivity : Activity
+    {
+        private Button myButton;
+        private WebView webView;
+        private Button buttonfileOpen;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+
+            myButton = FindViewById<Button>(Resource.Id.buttonClick);
+            buttonfileOpen = FindViewById<Button>(Resource.Id.buttonFileOpen);
+            webView = FindViewById<WebView>(Resource.Id.webView1);
+
+
+
+            myButton.Click += MyButton_ClickAsync;
+            buttonfileOpen.Click += ButtonfileOpen_Click;
+        }
+
+        private async void ButtonfileOpen_Click(object sender, EventArgs e)
+        {
+            var fn = "RR1.txt";
+            var file = Path.Combine(FileSystem.CacheDirectory, fn);
+            File.WriteAllText(file, "Keep Smile");
+
+            await Launcher.OpenAsync(new OpenFileRequest
+            {
+
+                File = new ReadOnlyFile(file)
+            });
+        }
+
+        private async void MyButton_ClickAsync(object sender, EventArgs e)
+        {
+            var supportsUri = await Launcher.CanOpenAsync("https://www.google.co.in");
+
+            if (supportsUri)
+            {
+                await Launcher.OpenAsync("https://www.google.co.in");
+            }
+
+        }
+
+    }
+}
